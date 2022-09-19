51d6d099ac6fdb1313a524d1050cae14bad804c8
vivek <viveksharmapoudel@gmail.com>
2022-09-19 17:57:58 +0545

    minor fix

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,17 +2,37 @@ import styled from "styled-components";
 import { media } from "../../styles/media";
 
 const Wrapper = styled.div`
-	width: 1200px;
-	border: 1px solid;
-	height: 300px;
-	background-color: blue;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-gap: 10px;
+	& > div {
+		width: 300px;
+		border: 1px solid;
+		height: 300px;
+		background-color: pink;
+	}
+
+	${media.md`
+    grid-template-columns:1fr 1fr ;
+  `}
 	${media.sm`
     width:100%;
+  	grid-template-columns: 1fr ;
+    & >div{
+
+    }
   `};
 `;
 
 const HomePage = () => {
-	return <Wrapper>homepage</Wrapper>;
+	return (
+		<Wrapper>
+			<div>column1</div>
+			<div>column2</div>
+			<div>column3</div>
+			<div>column4</div>
+		</Wrapper>
+	);
 };
 
 export default HomePage;
